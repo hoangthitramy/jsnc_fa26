d0445db7e68d45ee5fc518d6360d0b9f1e824ebb
HThitramy <myh46205@gmail.com>
2026-09-15 14:59:38 +0700

    first commit

---
+++ lab1.js
@@ -0,0 +1,9 @@
+let name = "Hoàng Thị Trà My";
+const age = 19;
+let address = "Thanh Hoa";
+let isStudent = true;
+
+console.log("Họ Tên: " + name);
+console.log("Tuổi: " + age);
+console.log("Địa chỉ: " + address);
+console.log("Sinh viên " + isStudent);
