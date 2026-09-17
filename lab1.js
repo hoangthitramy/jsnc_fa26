3ceca9ae7d3ab23c57833d3a6e294e501a1d2394
HThitramy <myh46205@gmail.com>
2026-09-17 15:15:49 +0700

    update code ngay 17-09

--- lab1.js
+++ lab1.js
@@ -1,9 +1,37 @@
-let name = "Hoàng Thị Trà My";
-const age = 19;
-let address = "Thanh Hoa";
+console.log(" BÀI 1 ");
+let studentName = "Nguyễn Văn An"; 
+let currentAge = 20;               
+let address = "Hà Nội";
 let isStudent = true;
 
-console.log("Họ Tên: " + name);
-console.log("Tuổi: " + age);
+console.log("Họ tên: " + studentName);
+console.log("Tuổi: " + currentAge);
 console.log("Địa chỉ: " + address);
-console.log("Sinh viên " + isStudent);
+console.log("Sinh viên: " + isStudent);
+
+console.log("\n BÀI 2 ");
+let age = 20;
+console.log(age); 
+
+age = 21;
+console.log(age); 
+
+console.log("\n BÀI 3 ");
+const a = 10;
+const b = 5;
+
+console.log("Tổng: " + (a + b));
+console.log("Hiệu: " + (a - b));
+console.log("Tích: " + (a * b));
+console.log("Thương: " + (a / b));
+
+
+console.log("\n BÀI 4 ");
+function sayHello(name) {
+    console.log("Xin chào " + name);
+}
+
+sayHello("An");
+sayHello("Bình");
+sayHello("Chi");
+sayHello("Đức");
