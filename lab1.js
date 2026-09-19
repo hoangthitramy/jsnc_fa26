cc640d2396f6bf450dcea79b4be0212738073c77
HThitramy <myh46205@gmail.com>
2026-09-19 15:22:13 +0700

    update code lab 1 ngay 19/9

--- lab1.js
+++ lab1.js
@@ -1,7 +1,8 @@
-console.log(" BÀI 1 ");
-let studentName = "Nguyễn Văn An"; 
-let currentAge = 20;               
-let address = "Hà Nội";
+console.log("BÀI 1");
+
+let studentName = "Hoang Thi Tra My";
+let currentAge = 19;
+let address = "Thanh Hoá";
 let isStudent = true;
 
 console.log("Họ tên: " + studentName);
@@ -9,14 +10,20 @@ console.log("Tuổi: " + currentAge);
 console.log("Địa chỉ: " + address);
 console.log("Sinh viên: " + isStudent);
 
-console.log("\n BÀI 2 ");
+
+console.log("\nBÀI 2");
+
 let age = 20;
-console.log(age); 
+
+console.log(age);
 
 age = 21;
-console.log(age); 
 
-console.log("\n BÀI 3 ");
+console.log(age);
+
+
+console.log("\nBÀI 3");
+
 const a = 10;
 const b = 5;
 
@@ -26,7 +33,8 @@ console.log("Tích: " + (a * b));
 console.log("Thương: " + (a / b));
 
 
-console.log("\n BÀI 4 ");
+console.log("\nBÀI 4");
+
 function sayHello(name) {
     console.log("Xin chào " + name);
 }
@@ -36,15 +44,30 @@ sayHello("Bình");
 sayHello("Chi");
 sayHello("Đức");
 
-console.log("\n BAI 5 ");
+
+console.log("\nBÀI 5");
+function sum(a, b) {
+    return a + b;
+}
+
 const result = sum(13, 23);
 console.log(result);
 
-console.log("\ BAI 6 ");
-function calculateTotal(price, quantity){
-    return calculateTotal = price * quantity;
-};
+
+console.log("\nBÀI 6");
+function calculateTotal(price, quantity) {
+    return price * quantity;
+}
+
 const total = calculateTotal(50000, 3);
 console.log(total);
 
-console.log("\ BAI 7 ");
+
+console.log("\nBÀI 7");
+function calculateAverage(math, english, javascript) {
+    return (math + english + javascript) / 3;
+}
+
+const average = calculateAverage(8, 7, 9);
+
+console.log(average);
